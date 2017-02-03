bc9f26f4af128656b14b0c089abc2f11d919533f
Zane Sandin <zane.sandin@uscis.dhs.gov>
2017-02-03 12:47:10 -0500

    Converted most code into module/class

--- hangman.rb
+++ hangman.rb
@@ -1,77 +1,49 @@
-#Array for all content
-words = []
-
-File.open("5desk.txt").each do |word|
-  words << word
-end
-
-secret_word = words.sample
-
-secret_word_blanks = Array.new(secret_word.length) { |x| x+1}
-secret_word_blanks.map! { |num| num = "_" }
-
-game_data = {
-  :alphabet => ('a'..'z').to_a,
-  :secret_word_letters => secret_word.split(""),
-  :revealed_letters => secret_word_blanks,
-  :correct_guesses => [],
-  :incorrect_guesses => [],
-  :turns => secret_word.length
-}
-
-def is_secret_word_letter(letter)
-  if secret_word_letters.include?(letter)
-    game_data[:correct_guesses] << letter
-    indices = []
-    game_data[:secret_word_letters].each do |x|
-      if x == letter
-        indices << game_data[:secret_word_letters].index(letter)
-      end
-    end
-    indices.each do |index|
-      game_data[:revealed_letters].map! { |x| x = letter if game_data[:revealed_letters].index(x) == index }
-    end
-  else
-    game_data[:incorrect_guesses] << letter
-  end
-end
-
-def show_progress
-  print game_data[:secret_word_letters]
-  puts ""
-  print game_data[:revealed_letters]
-  puts ""
-end
-
-def game_over?(turns, revealed_word_letters)
-end
-
-puts "Please type a letter to guess the content of the word"
-
-#Convert Progress so far into module/game
-
 module Hangman
   class Game
-
+    attr_accessor :alphabet, :secret_word, :secret_word_letters, :revealed_letters, :correct_guesses, :incorrect_guesses, :turns, :secret_word, :game_over
     def initialize
       words = []
       File.open("5desk.txt").each do |word|
         words << word
       end
+      #Alphabet to control user input
+      @alphabet = ('a'..'z').to_a
+      #Choose Random Word
       @secret_word = words.sample
       secret_word_blanks = Array.new(@secret_word.length) { |x| x+1}
       secret_word_blanks.map! { |num| num = "_" }
-      @alphabet = ('a'..'z').to_a
       @secret_word_letters = @secret_word.split("")
       @revealed_letters = secret_word_blanks
       @correct_guesses = []
       @incorrect_guesses = []
       @turns = @secret_word.length
-      @secret_word = []
-      @game_over = []
+      @game_over = false
     end
 
     def show_game_progress
+      print @secret_word_letters
+      puts ""
+      print @revealed_letters
+      puts ""
+    end
+
+    def is_secret_word_letter(letter)
+      if @secret_word_letters.include?(letter)
+        @correct_guesses << letter
+        indices = []
+        @secret_word_letters.each do |x|
+        if x == letter
+          indices << @secret_word_letters.index(letter)
+        end
+      end
+      indices.each do |index|
+        @revealed_letters.map! { |x| x = letter if @revealed_letters.index(x) == index }
+      end
+      else
+        incorrect_guesses << letter
+      end
+    end
+
     end
 
     def play_game
