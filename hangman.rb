d54e12b5c34a343f718cc91da35e14a7cbc89e2c
Zane Sandin <zane.sandin@uscis.dhs.gov>
2017-02-03 11:47:38 -0500

    Began converting everything to a module and class

--- hangman.rb
+++ hangman.rb
@@ -19,12 +19,6 @@ game_data = {
   :turns => secret_word.length
 }
 
-#This method should first see if it included with any of the secret word letters
-#If it is included, compare the revealed_letters array (which at the start will be
-#equal to "_" times the length of secret_word) with the secret_word_letters array.
-#and then make every "_" in the revealed_letters array equal to the index where the
-#letter occurs to the user choice letter
-
 def is_secret_word_letter(letter)
   if secret_word_letters.include?(letter)
     game_data[:correct_guesses] << letter
@@ -39,8 +33,14 @@ def is_secret_word_letter(letter)
     end
   else
     game_data[:incorrect_guesses] << letter
-
   end
+end
+
+def show_progress
+  print game_data[:secret_word_letters]
+  puts ""
+  print game_data[:revealed_letters]
+  puts ""
 end
 
 def game_over?(turns, revealed_word_letters)
@@ -48,32 +48,49 @@ end
 
 puts "Please type a letter to guess the content of the word"
 
-print game_data[:secret_word_letters]
-puts ""
-print game_data[:revealed_letters]
-puts ""
+#Convert Progress so far into module/game
 
-turn = 1
-while turn < game_data[:turns].length
-  puts "Please guess a letter in the word."
-  puts ""
-  user_choice = gets.chomp.to_s.downcase!
-  until game_data[:alphabet].include? user_choice and user_choice.length == 1 do
-    puts "Please choose a single letter in the alphabet to guess the content of the word."
-    user_choice = gets.chomp.to_s.downcase!
+module Hangman
+  class Game
+
+    def initialize
+      words = []
+      File.open("5desk.txt").each do |word|
+        words << word
+      end
+      @secret_word = words.sample
+      secret_word_blanks = Array.new(@secret_word.length) { |x| x+1}
+      secret_word_blanks.map! { |num| num = "_" }
+      @alphabet = ('a'..'z').to_a
+      @secret_word_letters = @secret_word.split("")
+      @revealed_letters = secret_word_blanks
+      @correct_guesses = []
+      @incorrect_guesses = []
+      @turns = @secret_word.length
+      @secret_word = []
+      @game_over = []
+    end
+
+    def show_game_progress
+    end
+
+    def play_game
+      turn = 1
+      while turn < game_data[:turns]
+        puts "Please guess a letter in the word."
+        puts ""
+        user_choice = gets.chomp.to_s
+        until game_data[:alphabet].include? user_choice and user_choice.length == 1 do
+          puts "Please choose a single letter in the alphabet to guess the content of the word."
+          user_choice = gets.chomp.to_s
+        end
+        puts "Evaluating user choice"
+        is_secret_word_letter(user_choice)
+        show_progress()
+        turn +=1
+      end
+    end
+
   end
-  is_secret_word_letter(user_choice)
-  #Logic here
-  #turn +=1
-
-
-
 end
-
-until user_choice.length == 1 and game_data[:alphabet].include? user_choice do
-  puts "Please guess a single letter."
-  user_choice = gets.chomp.to_s.downcase!
-end
-
-puts "You chose #{user_choice}"
 
